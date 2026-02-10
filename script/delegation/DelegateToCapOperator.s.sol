6f8cfb94d1df745ef452c27de61e29574151d41f
danoctavian <danoctavian91@gmail.com>
2026-02-11 01:23:01 +0200

    add expiry stuff

--- script/delegation/DelegateToCapOperator.s.sol
+++ script/delegation/DelegateToCapOperator.s.sol
@@ -12,6 +12,9 @@ import {IStakingNode} from "src/interfaces/IStakingNode.sol";
 import {ISignatureUtilsMixinTypes} from "lib/eigenlayer-contracts/src/contracts/interfaces/ISignatureUtilsMixin.sol";
 import {IStakingNodesManager} from "src/interfaces/IStakingNodesManager.sol";
 import {ContractAddresses} from "script/ContractAddresses.sol";
+import {IEigenOperator} from "script/delegation/IEigenOperator.sol";
+import {IEigenServiceManager} from "script/delegation/IEigenServiceManager.sol";
+import {EIGEN_SERVICE_MANAGER} from "script/delegation/Contracts.sol";
 
 contract DelegateToCapOperator is BaseScript {
 
@@ -46,20 +49,29 @@ contract DelegateToCapOperator is BaseScript {
         operators[operatorIndex++] = 0x4668d41D944B92f800965266D6382EF3F5C6B763;
 
         for (uint i = 0; i < stakingNodes.length; i++) {
-            address currentOperator = operators[i];
-
-            // Generate tx data for delegating to an operator
-            bytes memory delegateTxData = abi.encodeWithSelector(
-                IStakingNode.delegate.selector,
-                currentOperator,
-                ISignatureUtilsMixinTypes.SignatureWithExpiry({signature: "", expiry: 0}),
-                bytes32(0)
-            );
-            console.log("Node address:", stakingNodes[i]);
-            console.log("Index:", i);
-            console.log("Delegating to operator:", currentOperator);
-            console.log("Delegate transaction data:", vm.toString(abi.encodePacked(delegateTxData)));
+            if (operators[i] == address(0)) continue;
+            _buildDelegateTx(stakingNodes[i], operators[i], i);
         }
     }
 
+    function _buildDelegateTx(address node, address operator, uint256 i) internal {
+        address eigenOperatorAddr = operator;
+        uint256 expiryTimestamp = IEigenOperator(eigenOperatorAddr).getCurrentTotpExpiryTimestamp();
+        bytes32 salt = bytes32(expiryTimestamp);
+
+        bytes memory delegateTxData = abi.encodeWithSelector(
+            IStakingNode.delegate.selector,
+            operator,
+            ISignatureUtilsMixinTypes.SignatureWithExpiry({signature: "", expiry: expiryTimestamp}),
+            salt
+        );
+
+        console.log("Node address:", node);
+        console.log("Index:", i);
+        console.log("Delegating to operator:", operator);
+        console.log("EigenOperator:", eigenOperatorAddr);
+        console.log("Expiry timestamp:", expiryTimestamp);
+        console.log("Salt:", vm.toString(salt));
+        console.log("Delegate transaction data:", vm.toString(abi.encodePacked(delegateTxData)));
+    }
 }
